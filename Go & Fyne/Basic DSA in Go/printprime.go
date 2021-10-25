db6bd2fd9bac331fb33830221ba342f5a31c17ec
Shivam <shivampratapyadav@gmail.com>
2021-10-26 04:10:36 +0530

    Basic DSA Ques and Calculator Project

---
+++ Go & Fyne/Basic DSA in Go/printprime.go
@@ -0,0 +1,44 @@
+//Print Prime Numbers in a a given range:
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+
+func printPrimeNumbers( num1 , num2 int){
+	if num1<2 || num2<2{
+		fmt.Println("Numbers must be greater than 2.")
+		return
+	}
+
+	for num1<=num2{
+		isPrime := true
+
+		for i:=2 ; i<=int(math.Sqrt(float64(num1))) ; i++{
+			if num1 % i ==0{
+
+				isPrime = false
+
+				break
+			}
+		}
+
+		if isPrime{
+			fmt.Printf("%d " , num1)
+		}
+		
+		num1++
+	}
+
+	fmt.Println()
+}
+
+func main(){
+
+	printPrimeNumbers(5,15)
+	printPrimeNumbers(3,100)
+
+} 
